ff8ebb4cd2cfac637b198ec88ba3758463b19e21
Fernando Aragão <fernandoarag@gmail.com>
2021-10-13 19:05:35 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	title = 'course-manager';
-
-	name: string = 'Fernando';
+  title = 'course-manager';
+  name: string = 'Fernando';
 }
